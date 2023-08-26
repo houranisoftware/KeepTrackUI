e42da1fa6e11400c128ab3bdcd4ce70f689e363d
Hussein Hourani <houranisoftware@gmail.com>
2023-08-26 00:13:52 -0400

    Initialize dashboard tab navigator

---
+++ src/screens/DashBoard.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function DashBoard() {
+  return (
+    <View>
+      <Text>DashBoard</Text>
+    </View>
+  );
+}
+
+export default DashBoard;
