0066466b45c5b47656cbd53d667cdbd2ef3df063
Hussein Hourani <houranisoftware@gmail.com>
2023-08-26 02:26:00 -0400

    Create a My Week Tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import DashBoard from './screens/DashBoard';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faHouse, faUser, faDumbbell} from '@fortawesome/free-solid-svg-icons';
+import {
+  faHouse,
+  faUser,
+  faDumbbell,
+  faCalendarWeek,
+} from '@fortawesome/free-solid-svg-icons';
+
+import DashBoard from './screens/DashBoard';
 import UserProfile from './screens/UserProfile';
 import Workout from './screens/Workout';
+import MyWeek from './screens/MyWeek';
 
 const Tab = createBottomTabNavigator();
 
@@ -18,6 +25,15 @@ export default function App() {
           component={DashBoard}
           options={{
             tabBarIcon: () => <FontAwesomeIcon icon={faHouse} size={30} />,
+          }}
+        />
+        <Tab.Screen
+          name="My Week"
+          component={MyWeek}
+          options={{
+            tabBarIcon: () => (
+              <FontAwesomeIcon icon={faCalendarWeek} size={30} />
+            ),
           }}
         />
         <Tab.Screen
