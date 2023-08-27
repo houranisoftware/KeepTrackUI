c30a81f23c86f647737d87aa3ee991eb39d129bc
Hussein Hourani <houranisoftware@gmail.com>
2023-08-26 20:24:06 -0400

    Fix Provider issue

--- index.js
+++ index.js
@@ -5,13 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
-import {Provider} from 'react-redux';
-import store from './src/redux/store'; // Import your Redux store configuration
 
-const ReduxApp = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
-
-AppRegistry.registerComponent(appName, () => ReduxApp);
+AppRegistry.registerComponent(appName, () => App);
--- src/App.tsx
+++ src/App.tsx
@@ -13,44 +13,48 @@ import DashBoard from './screens/DashBoard';
 import UserProfile from './screens/UserProfile';
 import Workout from './screens/Workout';
 import MyWeek from './screens/MyWeek';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName="Dashboard">
-        <Tab.Screen
-          name="Dashboard"
-          component={DashBoard}
-          options={{
-            tabBarIcon: () => <FontAwesomeIcon icon={faHouse} size={30} />,
-          }}
-        />
-        <Tab.Screen
-          name="My Week"
-          component={MyWeek}
-          options={{
-            tabBarIcon: () => (
-              <FontAwesomeIcon icon={faCalendarWeek} size={30} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Workouts"
-          component={Workout}
-          options={{
-            tabBarIcon: () => <FontAwesomeIcon icon={faDumbbell} size={30} />,
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={UserProfile}
-          options={{
-            tabBarIcon: () => <FontAwesomeIcon icon={faUser} size={30} />,
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Dashboard">
+          <Tab.Screen
+            name="Dashboard"
+            component={DashBoard}
+            options={{
+              tabBarIcon: () => <FontAwesomeIcon icon={faHouse} size={30} />,
+            }}
+          />
+          <Tab.Screen
+            name="My Week"
+            component={MyWeek}
+            options={{
+              tabBarIcon: () => (
+                <FontAwesomeIcon icon={faCalendarWeek} size={30} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Workouts"
+            component={Workout}
+            options={{
+              tabBarIcon: () => <FontAwesomeIcon icon={faDumbbell} size={30} />,
+            }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={UserProfile}
+            options={{
+              tabBarIcon: () => <FontAwesomeIcon icon={faUser} size={30} />,
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
